eae4ffe36037b533578c23b23abee19d982c6d19
jesusfonser <jesusfonseruma@gmail.com>
2025-04-06 21:01:41 +0200

    Añade scripts y separa archivos configuracion Webpack

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
+
 # todo-list
 Cuarto proyecto del módulo JavaScript de The Odin Project. Se nos pide desarrollar una lista de tareas online utilizando todo lo aprendido hasta el momento.
---
+++ webpack.dev.js
@@ -0,0 +1,25 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devtool: "eval-source-map",
+  devServer: {
+    static: './dist',
+    watchFiles: ["./src/template.html"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
+    ],
+  }
+}
+);
