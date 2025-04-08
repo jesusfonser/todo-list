412f700dc1bf68d3489763d66683891ef0fc5ca7
jesusfonser <jesusfonseruma@gmail.com>
2025-04-08 16:31:29 +0200

    Instala date-fns y crea clase toDo

---
+++ src/todo.js
@@ -0,0 +1,11 @@
+class toDo{
+    done=false;
+    
+    constructor(title, description, dueDate, priority){
+        this.title=title;
+        this.description=description;
+        this.dueDate=dueDate;
+        this.priority=priority;
+        this.notes=notes;
+    }
+};
