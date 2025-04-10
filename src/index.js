231c9ecda7162ef5eef952697be3fe5c56203f91
jesusfonser <jesusfonseruma@gmail.com>
2025-04-10 14:43:13 +0200

    Añade texto y genera en JS botón para añadir proyectos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./style.css";
+import iconProject from "./imgs/note-text.svg";
+import iconAddProject from "./imgs/plus-circle-outline.svg";
+
+const addpro = document.querySelector(".addproyecto");
+
+const imgadd = document.createElement("img");
+imgadd.src=iconAddProject;
+
+addpro.appendChild(imgadd);
+addpro.innerHTML += "Añadir proyecto";
+
+
+
+
