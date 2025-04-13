d008fda5fee4009ed6d5b2fe3f3df3f775b8b61c
jesusfonser <jesusfonseruma@gmail.com>
2025-04-13 19:05:55 +0200

    Haz que proyectos muestren su contenido en pantalla

--- src/dom.js
+++ src/dom.js
@@ -15,6 +15,38 @@ export function project2DOM(p){
     botonP.innerHTML += p.title;
     botonP.setAttribute("class", "butt-menu");
     //Añadir eventListener para poner toda la info en display
-
+    botonP.addEventListener("click", () => displayProject(p));
     menuProjects.appendChild(botonP);
+    
+}
+
+function displayProject(q){
+    display.innerHTML = '';
+    
+    text2display("h2", q.title);
+    text2display("p", q.description + "<br><br>");
+    text2display("p", "Fecha límite: " + q.dueDate + "<br><br>");
+    text2display("h2", "Tareas");
+/*
+    const titulo_h2 = document.createElement("h2");
+    titulo_h2.innerText = q.title;
+    display.appendChild(titulo_h2);
+
+    const descripcion_p = document.createElement("p");
+    descripcion_p.innerText = q.description;
+    display.appendChild(descripcion_p);
+
+    const fecha_p = document.createElement("p");
+    fecha_p.innerHTML = "Fecha límite: " + q.dueDate + "<br><br>";
+    display.appendChild(fecha_p);
+
+    const tareas_h2 = document.createElement("h2");
+    tareas_h2.innerText = "Tareas";
+    display.appendChild(tareas_h2); */
+}
+
+function text2display(a, b){
+    const toadd = document.createElement(a);
+    toadd.innerHTML = b;
+    display.appendChild(toadd);
 }
