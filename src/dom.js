6a4baa10c5886480a092457099272e2502809283
jesusfonser <jesusfonseruma@gmail.com>
2025-04-14 18:47:57 +0200

    Arregla bug y estiliza mejor listas de tareas
    El bug permitía generar en menu listas con las mismas tareas haciendo click
    en el proyecto.

--- src/dom.js
+++ src/dom.js
@@ -14,9 +14,19 @@ export function project2DOM(p){
     botonP.appendChild(icono);
     botonP.innerHTML += p.title;
     botonP.setAttribute("class", "butt-menu");
-    //Añadir eventListener para poner toda la info en display
-    botonP.addEventListener("click", () => displayProject(p));
-    menuProjects.appendChild(botonP);
+    
+
+    const divProject = document.createElement("div");
+    divProject.setAttribute("class", "tareas-menu");
+    divProject.appendChild(botonP);
+
+    //EventListener para poner toda la info en display
+    botonP.addEventListener("click", () =>{ 
+        displayProject(p);
+        tasks2menu(p, divProject);
+    })
+
+    menuProjects.appendChild(divProject);
     
 }
 
@@ -31,13 +41,16 @@ function displayProject(q){
 //Aquí debería ir un bucle para meter todas las tareas del proyecto
 
     const ulTasksDisplay = document.createElement("ul");
+    ulTasksDisplay.setAttribute("class", "ul-tasks-display");
 
     q.toDos.forEach((x) =>{
         const task = document.createElement("li");
+        task.setAttribute("class", "task-display");
         const pTask = document.createElement("p");
         
         pTask.innerHTML = "<b>" + x.title + "</b>: " + x.description + "<br><br>Fecha límite: <i>" + x.dueDate + "</i> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Prioridad: <i>" + x.priority +"</i>";
         task.appendChild(pTask);
+
         ulTasksDisplay.appendChild(task);
     })
 
@@ -55,6 +68,22 @@ function text2display(a, b){
     display.appendChild(toadd);
 }
 
-function startShowProj(){
-    
+function tasks2menu(q, div){
+
+    const limpiaDivs = Array.from(document.querySelectorAll(".task-menu"));
+    limpiaDivs.forEach((x) => x.innerHTML = '');
+
+    const lista = document.createElement("ul");
+    lista.setAttribute("class", "task-menu");
+
+    q.toDos.forEach((x) => {
+        const li_tarea = document.createElement("li");
+        const button_tarea = document.createElement("button");
+
+        button_tarea.innerText=x.title;
+        button_tarea.setAttribute("class", "butt-menu");
+        li_tarea.appendChild(button_tarea);
+        lista.appendChild(li_tarea);
+    })
+    div.appendChild(lista);
 }
